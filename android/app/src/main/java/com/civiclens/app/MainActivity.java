90e9e190bba527ad7e906082abf3c78729e82a2c
Joseph Kannan <252888548+josephkannan1011-dev@users.noreply.github.com>
2026-07-07 23:34:48 +0530

    Final updation

---
+++ android/app/src/main/java/com/civiclens/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.civiclens.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.civiclens.app',
+  appName: 'civiclens-ai',
+  webDir: 'dist'
+};
+
+export default config;
